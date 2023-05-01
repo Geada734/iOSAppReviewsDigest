4cf33ca3f5927eb56fea515460e43b9c9b2c95a5
Geada734 <frankgeada@gmail.com>
2023-04-30 21:11:51 -0600

    Code cleanup for the server

--- server/app/app.py
+++ server/app/app.py
@@ -1,27 +1,38 @@
+'''Flask Backend for the iOS Reviews Digest app'''
 import json
+from flask_cors import CORS
 from utils import get_reviews
 from flask import Flask, request
 from flask_restful import Resource, Api
-from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-'''Flask Backend'''
+'''Reviews Endpoints'''
 class AppReviews(Resource):
+    '''
+    Endpoint to get all the reviews for a given app
+    on a given page.
+    '''
     def get(self):
-        # 284882215 for facebook
-        # 835599320 for tiktok
         app_id = request.args.get('app_id')
         page = request.args.get('page')
-        # Utility function to call the necessary endpoints.
+        # Utility function to call the RSS feed endpoint
+        # and save the app's state.
         response = get_reviews(app_id, page)
 
         return response
 
+'''App State's Endpoints'''
 class AppState(Resource):
+    '''
+    Gets the current app's state based on the last 
+    backend call.
+    '''
     def get(self):
+        # Retrieves the app's state from the json file
+        # in the 'store' folder.
         state_file = open("./store/state.json", "r")
         response = json.load(state_file)
         state_file.close()
@@ -32,5 +43,5 @@ api.add_resource(AppReviews, '/appReviews')
 api.add_resource(AppState, '/appState')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
     
